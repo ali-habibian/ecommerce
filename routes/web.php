484b0ff665e6778c957980ba9c9fafdf3d561c9b
Ali Habibian <ali.habibian04@gmail.com>
2024-08-25 22:47:46 +0330

    feat: Implement basic admin dashboard with authorization
    - Created `HomeController` and tests for admin dashboard access.
    - Added route and middleware for protecting admin dashboard.
    - Implemented checks for a super admin role and permission-based access.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')
+    ->middleware(['auth', 'permission:view admin dashboard'])
+    ->name('admin.')
+    ->group(static function () {
+        Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('home.index');
+    });
