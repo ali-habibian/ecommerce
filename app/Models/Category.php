f80d4b8f883e6bee1238440e711523b0fa78a42c
Ali Habibian <ali.habibian04@gmail.com>
2024-09-01 15:28:02 +0330

    feat: Implement Persian slug support, Category model, factory and seed categories
    - Added `pishran/laravel-persian-slug` package for generating Persian slugs in models
    - Integrated Persian slug generation in the `category` model using the `HasPersianSlug` trait
    - Defined `CategoryFactory` with Persian locale Faker for generating fake category data
    - Added parent-child relationship methods (`parent` and `children`) to the `Category` model
    - Created `CategorySeeder` to populate the database with sample category data for testing

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use phpDocumentor\Reflection\Types\This;
+use Pishran\LaravelPersianSlug\HasPersianSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasPersianSlug;
 
     /**
      * The attributes that are not mass assignable.
@@ -15,4 +20,31 @@ class Category extends Model
      * @var array
      */
     protected $guarded = [];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Retrieve the parent category that this category belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    /**
+     * Retrieve the child categories that belong to this category.
+     *
+     * @return HasMany
+     */
+    public function children(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,14 +12,36 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CategoryFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Category::class;
+
+    /**
+     * The Faker instance with the Persian locale.
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
+        // Create a Faker instance with the Persian locale
+        $this->faker = FakerFactory::create('fa_IR');
+
         return [
-            //
+            'name' => $this->faker->unique()->word,
+            'parent_id' => $this->faker->randomElement([
+                Category::factory(),
+                null,
+            ]),
+            'description' => $this->faker->sentence,
         ];
     }
 }
