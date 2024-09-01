dbf0be501bee63276589122af96d78b16c576834
Ali Habibian <ali.habibian04@gmail.com>
2024-09-01 15:58:43 +0330

    fix: Improve Persian `name` and `description` generation in CategoryFactory
    - Added `kasraghoreyshi/persian-faker` package to enhance Persian text generation
    - Updated `CategoryFactory` to use `persianWords` and `persianSentence` methods for generating Persian `name` and `description`

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\Category;
-use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -19,29 +18,19 @@ class CategoryFactory extends Factory
     protected $model = Category::class;
 
     /**
-     * The Faker instance with the Persian locale.
-     *
-     * @var \Faker\Generator
-     */
-    protected $faker;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
-        // Create a Faker instance with the Persian locale
-        $this->faker = FakerFactory::create('fa_IR');
-
         return [
-            'name' => $this->faker->unique()->word,
+            'name' => $this->faker->unique()->persianWords($nb = 3, $asText = true),
             'parent_id' => $this->faker->randomElement([
                 Category::factory(),
                 null,
             ]),
-            'description' => $this->faker->sentence,
+            'description' => $this->faker->persianSentence($nb = 3, $variableNbWords = true),
         ];
     }
 }
