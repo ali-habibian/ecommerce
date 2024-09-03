3363ea8a4aceef04a6e93ae6cb1c3a882e2caf25
Ali Habibian <ali.habibian04@gmail.com>
2024-09-04 01:00:22 +0330

    Added JQuery library to the project and CSRF-TOKEN to ajax requests

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
 import './bootstrap';
 import './elements/turbo-echo-stream-tag';
 import './libs';
+import jQuery from 'jquery';
+window.$ = jQuery;
+import Swal from "sweetalert2";
+window.Swal = Swal
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,13 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
           name="viewport">
-    <meta name="csrf-token"
-          content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>دیجی ارومیه - @yield('title')</title>
 
     <link rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
           crossorigin="anonymous">
-{{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">--}}
-{{--    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>--}}
-{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>--}}
-{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>--}}
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -39,6 +34,16 @@
     </div>
 </div>
 @stack('modals')
+<script type="module">
+    // Set up the CSRF token for all AJAX requests
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
+
+@stack('scripts')
 </body>
 
 </html>
