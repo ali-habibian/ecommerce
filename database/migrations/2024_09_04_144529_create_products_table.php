80d3ef542d6e22d9babb1dcc3a573b5186b243a3
Ali Habibian <ali.habibian04@gmail.com>
2024-09-04 18:21:31 +0330

    feat: Implemented Product migration

--- database/migrations/2024_09_04_144529_create_products_table.php
+++ database/migrations/2024_09_04_144529_create_products_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('sku');
+            $table->decimal('price', 10);
+            $table->decimal('discounted_price', 10)->default(0);
+            $table->decimal('cost', 10)->default(0);
+            $table->integer('quantity')->default(0);
+            $table->boolean('track_quantity')->default(true);
+            $table->boolean('sell_out_of_stock')->default(false);
+            $table->text('description')->nullable();
+            $table->string('status');
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->onDelete('set null');
             $table->timestamps();
         });
     }
