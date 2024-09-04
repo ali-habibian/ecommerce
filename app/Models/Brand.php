54bb49f40d7d01f4353acb7cd982d28fbe579d09
Ali Habibian <ali.habibian04@gmail.com>
2024-09-04 16:11:32 +0330

    feat: Implement Brand model with Scout, Sluggable, and factory
    - Added Laravel Scout and Spatie Sluggable configuration to the Brand model.
    - Implemented slug generation based on the 'name' attribute.
    - Added a custom search scope (whereScout) for the Brand model to support searching with Laravel Scout.
    - Updated the BrandFactory to generate test data, including name and description fields.

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,10 +2,58 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+use Pishran\LaravelPersianSlug\HasPersianSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Brand extends Model
 {
-    use HasFactory;
+    use HasFactory, HasPersianSlug, Searchable;
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+        ];
+    }
+
+    /**
+     * Scope a query to only include listings that are returned by scout
+     *
+     * @param Builder $query
+     * @param string $search
+     * @return Builder
+     */
+    public function scopeWhereScout(Builder $query, string $search): Builder
+    {
+        return $query->whereIn(
+            'id',
+            self::search($search)
+                ->get()
+                ->pluck('id'),
+        );
+    }
 }
--- database/factories/BrandFactory.php
+++ database/factories/BrandFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Brand;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,6 +11,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class BrandFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Brand::class;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -17,7 +25,8 @@ class BrandFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->persianWords(),
+            'description' => $this->faker->persianParagraph(),
         ];
     }
 }
