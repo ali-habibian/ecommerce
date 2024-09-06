16a54b8f014d362c5600719327f22a9d8468604e
Ali Habibian <ali.habibian04@gmail.com>
2024-09-06 18:19:12 +0330

    feat: Integrate Bootstrap Ecommerce theme and setup assets
    - Download and extract Bootstrap Ecommerce theme stylesheets to resources/sass
    - Create _store-variables.scss for Bootstrap customization
    - Add JavaScript file resources/js/store.js to register turbo and stimulus
    - Update vite.config.js to compile new styles and scripts

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+                'resources/js/store.js',
+                'resources/sass/store.scss',
+            ],
             refresh: true,
         }),
     ],
